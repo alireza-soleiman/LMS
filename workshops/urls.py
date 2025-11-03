c8d3349c8eb21aef82d9307b3253e6f7efc1e7a3
alireza-soleiman <alireza.soleyman.b@gmail.com>
2025-11-03 17:12:30 +0100

    error fix

--- workshops/urls.py
+++ workshops/urls.py
@@ -9,7 +9,6 @@ urlpatterns = [
     path('project/<int:project_id>/stakeholders/data/', views.stakeholder_data, name='stakeholder_data_api'),
     path('project/<int:project_id>/problem-tree/', views.problem_tree_view, name='problem_tree'),
     path('stakeholder/update/<int:stakeholder_id>/', views.update_stakeholder_details, name='update_stakeholder_details'),
-    path('project/<int:project_id>/problem-tree/download/png/', views.download_problem_tree_png, name='download_problem_tree_png'),
     path('problem/delete/<int:problem_id>/', views.delete_problem, name='delete_problem'),
     path('api/project/<int:project_id>/problem-data/', views.problem_tree_data, name='problem_tree_data_api'),
     path('stakeholder/delete/<int:stakeholder_id>/', views.delete_stakeholder, name='delete_stakeholder'),
