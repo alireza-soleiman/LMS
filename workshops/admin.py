4a5d8b4be14b290329b800ff74f5d004deabe8a5
alireza-soleiman <alireza.soleyman.b@gmail.com>
2025-11-04 17:33:42 +0100

    add admin panel for indicators

--- workshops/admin.py
+++ workshops/admin.py
@@ -1,8 +1,21 @@
 from django.contrib import admin
-from .models import Project, Stakeholder, Problem
+from .models import Project, Stakeholder, Problem , MasterIndicator, Indicator
 
 # This "registers" our models, making them visible and manageable
 # in the admin interface.
 admin.site.register(Project)
 admin.site.register(Stakeholder)
 admin.site.register(Problem)
+
+
+@admin.register(MasterIndicator)
+class MasterIndicatorAdmin(admin.ModelAdmin):
+    list_display = ("category", "criterion", "name", "unit")
+    search_fields = ("name", "criterion", "description")
+    list_filter = ("category",)
+
+@admin.register(Indicator)
+class IndicatorAdmin(admin.ModelAdmin):
+    list_display = ("project", "name", "criterion", "accepted", "added_by_student")
+    search_fields = ("name", "criterion", "description", "project__title")
+    list_filter = ("accepted", "project", "category")
