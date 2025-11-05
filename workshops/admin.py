e0ec7c1752a654b8fd34ecc7fa7296204e5fd17f
alireza-soleiman <alireza.soleyman.b@gmail.com>
2025-11-05 18:05:18 +0100

    add swot to admin panel

--- workshops/admin.py
+++ workshops/admin.py
@@ -19,3 +19,12 @@ class IndicatorAdmin(admin.ModelAdmin):
     list_display = ("project", "name", "criterion", "accepted", "added_by_student")
     search_fields = ("name", "criterion", "description", "project__title")
     list_filter = ("accepted", "project", "category")
+
+
+from .models import SWOTItem
+
+@admin.register(SWOTItem)
+class SWOTItemAdmin(admin.ModelAdmin):
+    list_display = ('project', 'category', 'title', 'created_at')
+    list_filter = ('category', 'project')
+    search_fields = ('title', 'description')
